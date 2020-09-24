5c23b4429c9c85e6ffa786b951e8b5093ad7a19d
cezaroliveira <cezaroliveira1987@gmail.com>
2020-09-24 11:26:44 -0300

    Passing an argument to MySQL to compatibility with mysqljs library in Node.

--- docker-run-with-volume-mysql.cmd
+++ docker-run-with-volume-mysql.cmd
@@ -1,2 +1,4 @@
 @REM Executa uma imagem no docker montando um volume
-docker run -d -v %cd%/api/db/data:/var/lib/mysql --rm --name mysql-container mysql-image
+@REM Erro: Client does not support authentication protocol requested by server; consider upgrading MySQL client
+@REM Solução: Passar o argumento --default-authentication-plugin=mysql_native_password para o MySQL
+docker run -d -v %cd%/api/db/data:/var/lib/mysql --rm --name mysql-container mysql-image --default-authentication-plugin=mysql_native_password
