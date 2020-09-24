bf81559f2a5a559080a95d5065e91af27fae94cb
cezaroliveira <cezaroliveira1987@gmail.com>
2020-09-24 08:57:35 -0300

    Renaming command files.

---
+++ docker-run-with-volume-mysql.cmd
@@ -0,0 +1,2 @@
+@REM Executa uma imagem no docker montando um volume
+docker run -d -v %cd%/api/db/data:/var/lib/mysql --rm --name mysql-container mysql-image
--- docker-run-with-volume.cmd
+++
@@ -1,2 +0,0 @@
-@REM Executa uma imagem no docker montando um volume
-docker run -d -v %cd%\api\db\data:/var/lib/mysql --rm --name mysql-container mysql-image
